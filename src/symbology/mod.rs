52ca785af1c825f427a78fd28a625d9278d81344
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2023-11-10 17:58:07 -0600

    add coinbase loader

--- src/symbology/mod.rs
+++ src/symbology/mod.rs
@@ -1,6 +1,6 @@
 use crate::hcstatic;
 use allocator::StaticBumpAllocator;
-use anyhow::bail;
+use anyhow::{bail, Result};
 use api::{symbology::Symbolic, Str};
 use arc_swap::ArcSwap;
 use hcstatic::Hcstatic;
@@ -20,3 +20,39 @@ hcstatic!(Venue, api::symbology::Venue, 64);
 hcstatic!(Route, api::symbology::Route, 64);
 hcstatic!(Product, api::symbology::Product, 512);
 hcstatic!(Market, api::symbology::Market, 512);
+
+// forward mount the [new] impls as a convenience
+
+impl Venue {
+    pub fn new(name: &str) -> Result<api::symbology::Venue> {
+        api::symbology::Venue::new(name)
+    }
+}
+
+impl Route {
+    pub fn new(name: &str) -> Result<api::symbology::Route> {
+        api::symbology::Route::new(name)
+    }
+}
+
+impl Product {
+    pub fn new(
+        name: &str,
+        kind: api::symbology::ProductKind,
+    ) -> Result<api::symbology::Product> {
+        api::symbology::Product::new(name, kind)
+    }
+}
+
+impl Market {
+    pub fn new(
+        name: &str,
+        kind: api::symbology::MarketKind,
+        venue: &api::symbology::Venue,
+        route: &api::symbology::Route,
+        exchange_symbol: &str,
+        extra_info: api::symbology::MarketInfo,
+    ) -> Result<api::symbology::Market> {
+        api::symbology::Market::new(name, kind, venue, route, exchange_symbol, extra_info)
+    }
+}
