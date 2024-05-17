cbed173279b18ab507e9a38b6e2275137709a3dc
andrew-lee-architect <126016409+andrew-lee-architect@users.noreply.github.com>
2024-05-17 14:46:14 -0500

    GQL accounts admin stuff (3/3) (#447)

--- src/account_master.rs
+++ src/account_master.rs
@@ -1,7 +1,11 @@
 use crate::{ChannelDriver, Common};
 use anyhow::Result;
-use api::{expect_response, orderflow::AccountMessage, Account};
-use std::sync::Arc;
+use api::{
+    expect_response, orderflow::AccountMessage, Account, MessageTopic, TypedMessage,
+};
+use async_stream::stream;
+use futures::Stream;
+use std::{pin::Pin, sync::Arc};
 use uuid::Uuid;
 
 pub async fn get_accounts(
@@ -18,4 +22,40 @@ pub async fn get_accounts(
         )
         .await?;
     Ok(accounts)
+}
+
+pub type AccountStream = Pin<Box<dyn Stream<Item = Arc<Vec<Account>>> + Send>>;
+
+pub async fn subscribe_accounts(
+    common: &Common,
+    driver: &ChannelDriver,
+) -> Result<AccountStream> {
+    let common = common.clone();
+    let mut rx = driver.subscribe();
+    let com = common.find_component_of_kind("AccountMaster")?;
+    driver.subscribe_channel_to_topics(MessageTopic::Accounts.into())?;
+    let id = Uuid::new_v4();
+    let initial_accounts = driver
+        .request_and_wait_for(
+            com,
+            AccountMessage::GetAccounts(id),
+            expect_response!(AccountMessage::Accounts(_, accounts) => accounts),
+        )
+        .await?;
+    let stream = stream! {
+        // CR alee: probably want some way to re-query initial accounts if e.g. the core restarts
+        yield initial_accounts;
+        while let Ok(batch) = rx.recv().await {
+            for env in batch.iter() {
+                match &env.msg {
+                    TypedMessage::AccountMaster(AccountMessage::Accounts(None, accounts)) =>
+                    {
+                        yield accounts.clone();
+                    }
+                    _ => {}
+                }
+            }
+        }
+    };
+    Ok(Box::pin(stream))
 }
