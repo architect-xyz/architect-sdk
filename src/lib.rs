a4968075381bd3b0f68e98a6909671d211c770b4
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2025-02-07 13:17:34 -0600

    move some config around

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use anyhow::{bail, Result};
+use std::path::{Path, PathBuf};
+
 // pub mod client;
 #[cfg(feature = "graphql")]
 pub mod graphql;
@@ -13,3 +16,27 @@ pub mod synced;
 // pub use client::ArchitectClient;
 
 pub use marketdata::MarketdataSource;
+
+/// Canonical config file location resolution.
+pub fn locate_config(from_arg: Option<impl AsRef<Path>>) -> Result<PathBuf> {
+    const FROM_ENV: &[&str] = &["ARCHITECT_CFG"];
+    const DEFAULT_CONFIG: Option<&str> = Some("~/.architect/config.yml");
+    if let Some(path) = from_arg {
+        return Ok(path.as_ref().to_owned());
+    }
+    for env in FROM_ENV {
+        if let Ok(path) = std::env::var(env) {
+            return Ok(path.into());
+        }
+    }
+    if let Some(path) = DEFAULT_CONFIG {
+        if let Some(path) = path.strip_prefix("~/") {
+            if let Some(home_dir) = dirs::home_dir() {
+                return Ok(home_dir.join(path));
+            }
+        } else {
+            return Ok(path.to_owned().into());
+        }
+    }
+    bail!("config file was required but not specified");
+}
