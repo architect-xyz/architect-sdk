c067896d79f6a5bee9d4bea5168959f3066266b7
Brett Harrison <116864397+brett-harrison-architect@users.noreply.github.com>
2025-06-11 15:18:10 -0500

    Bharrison/options uat (#1098)
    # Description
    
    Freeform description, whatever you want to say about the PR. This may be
    empty for trivial changes.
    
    ## Changelog
    
    - [ ] Update `CHANGELOG.md` to reflect the changes in this PR
    
    ---------
    
    Co-authored-by: Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>

--- src/symbology/parsers.rs
+++ src/symbology/parsers.rs
@@ -6,6 +6,10 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 pub struct UsEquityOptionsParts {
+    // Architect Tradable Prodcut format:
+    // e.g. AAPL US 20260918 220.50 C Option/USD
+    // {underlying} US {exp YYYYMMDD} {strike, two decimals} {C|P} Option/USD
+    pub tradable_product: TradableProduct,
     // OSI 21-byte format:
     // SSSSSSYYMMDDOPPPPPppp
     // 0-5: symbol, right space padded
@@ -24,7 +28,9 @@ pub struct UsEquityOptionsParts {
 
 impl UsEquityOptionsParts {
     // Expceted base product format: AAPL US 20260918 220.50 C Option
-    pub fn parse(tradable_product: &TradableProduct) -> Result<Option<Self>> {
+    pub fn parse_tradable_product(
+        tradable_product: &TradableProduct,
+    ) -> Result<Option<Self>> {
         let base = tradable_product.base().to_string();
         if !(base.contains(" US ") && base.contains("Option")) {
             return Ok(None);
@@ -36,6 +42,63 @@ impl UsEquityOptionsParts {
         let underlying_symbol = parts[0].replace("-", ".");
         let expiration = NaiveDate::parse_from_str(&parts[2], "%Y%m%d")?;
         let strike_price = Decimal::from_str(&parts[3])?;
+        let put_or_call_char = parts[4];
+        let put_or_call = match put_or_call_char {
+            "C" => PutOrCall::Call,
+            "P" => PutOrCall::Put,
+            _ => bail!("Expected C or P, got {} for symbol {}", put_or_call_char, base),
+        };
+        let osi_symbol = Self::form_osi_symbol(
+            &underlying_symbol,
+            expiration,
+            strike_price,
+            put_or_call,
+        )?;
+        Ok(Some(Self {
+            tradable_product: tradable_product.clone(),
+            osi_symbol,
+            underlying_symbol,
+            expiration,
+            strike_price,
+            put_or_call,
+        }))
+    }
+
+    pub fn from_parts(
+        underlying_symbol: &str,
+        expiration: NaiveDate,
+        strike_price: Decimal,
+        put_or_call: PutOrCall,
+    ) -> Result<Self> {
+        let tradable_product = TradableProduct::from_str(&format!(
+            "{} US {} {} {} Option/USD",
+            underlying_symbol.replace(".", "-"),
+            expiration.format("%Y%m%d"),
+            strike_price,
+            put_or_call
+        ))?;
+        let osi_symbol = Self::form_osi_symbol(
+            underlying_symbol,
+            expiration,
+            strike_price,
+            put_or_call,
+        )?;
+        Ok(Self {
+            tradable_product,
+            osi_symbol,
+            underlying_symbol: underlying_symbol.to_string(),
+            expiration,
+            strike_price,
+            put_or_call,
+        })
+    }
+
+    pub fn form_osi_symbol(
+        underlying_symbol: &str,
+        expiration: NaiveDate,
+        strike_price: Decimal,
+        put_or_call: PutOrCall,
+    ) -> Result<String> {
         let strike_price_string = strike_price.to_string();
         let (strike_price_dollar_part, strike_price_decimal_part) =
             strike_price_string.split_once(".").ok_or_else(|| {
@@ -44,13 +107,11 @@ impl UsEquityOptionsParts {
                     strike_price
                 )
             })?;
-        let put_or_call_char = parts[4];
-        let put_or_call = match put_or_call_char {
-            "C" => PutOrCall::Call,
-            "P" => PutOrCall::Put,
-            _ => bail!("Expected C or P, got {} for symbol {}", put_or_call_char, base),
+        let put_or_call_char = match put_or_call {
+            PutOrCall::Put => "P",
+            PutOrCall::Call => "C",
         };
-        let osi_symbol = format!(
+        Ok(format!(
             "{:<6}{:02}{:02}{:02}{}{:0>5}{:0<3}",
             underlying_symbol,
             expiration.year().to_string().split_off(2),
@@ -59,14 +120,7 @@ impl UsEquityOptionsParts {
             put_or_call_char,
             strike_price_dollar_part,
             strike_price_decimal_part
-        );
-        Ok(Some(Self {
-            osi_symbol,
-            underlying_symbol,
-            expiration,
-            strike_price,
-            put_or_call,
-        }))
+        ))
     }
 }
 
@@ -78,7 +132,8 @@ mod tests {
     fn test_parse_option() -> Result<()> {
         let tradable_product =
             TradableProduct::from_str("AAPL US 20260918 220.50 C Option/USD")?;
-        let symbology = UsEquityOptionsParts::parse(&tradable_product)?.unwrap();
+        let symbology =
+            UsEquityOptionsParts::parse_tradable_product(&tradable_product)?.unwrap();
         assert_eq!(symbology.osi_symbol, "AAPL  260918C00220500");
         assert_eq!(symbology.underlying_symbol, "AAPL");
         assert_eq!(symbology.expiration, NaiveDate::from_ymd_opt(2026, 9, 18).unwrap());
